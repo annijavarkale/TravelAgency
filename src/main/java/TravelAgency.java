e58e857905cd4794f9cecda9cb0185538dcce89e
annijavarkale <annija.varkale@inbox.lv>
2022-10-16 15:23:36 +0300

    Added ability to plan a trip and save it in a file

--- src/main/java/TravelAgency.java
+++ src/main/java/TravelAgency.java
@@ -1,9 +1,20 @@
-import api_helpers.RestAPIHelper;
+import helpers.FileHelper;
 import locations.Country;
+import tickets.GroupTicket;
+import tickets.RegularTicket;
+import tickets.StudentTicket;
+import tickets.Ticket;
+import transport_types.Bicycle;
+import transport_types.Car;
+import transport_types.Plane;
+import transport_types.Transportation;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-import static api_helpers.RestAPIHelper.API_CALL_PREFIX;
+import static helpers.RestAPIHelper.API_CALL_PREFIX;
+import static helpers.RestAPIHelper.createCountryFromResponse;
+import static java.lang.Math.random;
 
 public class TravelAgency {
     public static void main(String[] args) {
@@ -37,7 +48,65 @@ public class TravelAgency {
     }
 
     private static void createTripRoute() {
+        int maxTicketPrice = 500;
+        int minTicketPrice = 100;
+        Ticket ticket = null;
+        Transportation transportation = null;
+        int numOfCountries;
+        ArrayList<Country> tripCountries  = new ArrayList<>();
 
+        Scanner scanner = new Scanner(System.in);
+        // Process ticket information
+        System.out.print("Please select trip price plan\n" +
+                "1 | Traveling alone\n" +
+                "2 | Traveling as a student\n" +
+                "3 | Traveling in a group\n" +
+                "---> ");
+        String pricePlan = scanner.nextLine().trim();
+        double ticketFullPrice = random() * (maxTicketPrice - minTicketPrice) + minTicketPrice;
+        switch (pricePlan){
+            case "1":
+                ticket = new RegularTicket(ticketFullPrice);
+                break;
+            case "2":
+                ticket = new StudentTicket(ticketFullPrice);
+                break;
+            case "3":
+                System.out.print("Please enter the amount of people in the group\n--->");
+                ticket = new GroupTicket(ticketFullPrice, Integer.parseInt(scanner.nextLine()));
+                break;
+        }
+
+        // Process ticket information
+        System.out.print("Please select the type of transport\n" +
+                "1 | Traveling by car\n" +
+                "2 | Traveling by bicycle\n" +
+                "3 | Traveling by plane\n" +
+                "---> ");
+        String transportType = scanner.nextLine().trim();
+        switch (transportType) {
+            case "1":
+                transportation = new Car();
+                break;
+            case "2":
+                transportation = new Bicycle();
+                break;
+            case "3":
+                transportation = new Plane();
+                break;
+        }
+        System.out.print("Please enter the amount of countries you want to visit:\n---->");
+        numOfCountries = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < numOfCountries; i++) {
+            System.out.print("Enter country name:\n-->");
+            String countryName = scanner.nextLine();
+            Country destinationCountry = createCountryFromResponse(API_CALL_PREFIX + "name/" + countryName);
+            destinationCountry.setTicket(ticket);
+            destinationCountry.setTransportation(transportation);
+            tripCountries.add(destinationCountry);
+        }
+        FileHelper.saveRouteToFile(tripCountries);
+        welcomeScreen();
     }
 
     private static void searchCountryByName() {
@@ -51,7 +120,8 @@ public class TravelAgency {
     private static void searchCountryByProperty(String property) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Please enter the %s of country:\n --->", property);
-        Country country = RestAPIHelper.createCountryFromResponse(API_CALL_PREFIX + property + "/" + scanner.nextLine().trim());
+        Country country = createCountryFromResponse(API_CALL_PREFIX + property + "/"
+                + scanner.nextLine().trim());
 
         country.printInfo();
         welcomeScreen();
